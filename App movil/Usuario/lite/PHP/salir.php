30a40ec7604d189fe40934e337657a7bef1c1994
Mariana Vallejo <marianavall2009@gmail.com>
2018-11-16 21:32:19 -0500

    Usuario

--- App movil/Usuario/lite/PHP/salir.php
+++
@@ -1,6 +0,0 @@
-<?php
-	session_start();
-  session_destroy();
-	print "<script>alert(\"Cerraste sesión.\");window.location='../../../login.php';</script>";
-	?>
-<script language="javascript">location.href = "../../../login.php";</script>

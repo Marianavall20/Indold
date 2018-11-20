4e31e0ca1fd4acb41d35306167de8e2d74f3ce78
DESKTOP-IPIO4VA\usuario <mcgrajales01@gmail.com>
2018-11-20 15:16:37 -0500

    utf

--- App movil/administrador/dompdf/index.php
+++ App movil/administrador/dompdf/index.php
@@ -8,6 +8,7 @@ $mysqli = new mysqli("localhost", "root", "", "recursos_humanos");
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <meta charset="utf-8">
 </head>
 <body>
 <h1>Hola</h1>

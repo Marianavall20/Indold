88c7771f07792cd2132dc2e3fbaa02d175f90b86
DESKTOP-6C93UEM\Asus PC <montoyapaola03@gmail.com>
2018-11-21 13:08:48 -0500

    consumo mes

--- Usuario/lite/PHP/graficas.php
+++ Usuario/lite/PHP/graficas.php
@@ -149,7 +149,7 @@
                 <!-- ============================================================== -->
                 <div class="row page-titles">
                     <div class="col-md-5 col-8 align-self-center">
-                        <h3 class="text-themecolor">Dashboard</h3>
+                        <h3 class="text-themecolor">Inicio</h3>
                         <ol class="breadcrumb">
                             <li class="breadcrumb-item"><a href="javascript:void(0)">Home</a></li>
                             <li class="breadcrumb-item active">Dashboard</li>

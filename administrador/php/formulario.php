40d829771168cbb08eb20b87bcf8a54ca7651b3b
DESKTOP-6C93UEM\Asus PC <montoyapaola03@gmail.com>
2018-11-21 13:30:05 -0500

    registro propietario

--- administrador/php/formulario.php
+++ administrador/php/formulario.php
@@ -9,7 +9,7 @@ if(isset($_SESSION['usuario'])) {?>
   <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>Registro trabajador</title>
+    <title>Registro trabaj</title>
     <meta name="description" content="">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="robots" content="all,follow">
